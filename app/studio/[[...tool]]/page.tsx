ca3a8f17c94163e16db24a887db7b6a114e379b3
marmuzzi <m.armuzzi@gmail.com>
2026-07-03 09:45:08 +0100

    Make Sanity Studio route client-only

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,10 +1,13 @@
-import { NextStudio } from "next-sanity/studio";
+"use client";
+
+import dynamic from "next/dynamic";
 
 import config from "@/sanity.config";
 
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((module) => module.NextStudio),
+  { ssr: false },
+);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
