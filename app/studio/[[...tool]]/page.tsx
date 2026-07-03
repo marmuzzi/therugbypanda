a3a03ee7f18199d43ca723ee1526dd178013963d
marmuzzi <m.armuzzi@gmail.com>
2026-07-03 10:37:27 +0100

    Replace NextStudio with Studio component

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { Studio } from "sanity";
 
 import config from "@/sanity.config";
 
-const NextStudio = dynamic(
-  () => import("next-sanity/studio").then((module) => module.NextStudio),
-  { ssr: false },
-);
-
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <Studio config={config} />;
 }
